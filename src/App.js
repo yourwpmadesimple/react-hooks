3e8e54db9d3193d1fa2161be3b2eb2e20647a568
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 02:27:41 -0500

    using props to alter data on component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello { props.name }</h1>
     </div>
   );
 }
