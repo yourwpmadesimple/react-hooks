12e194453912561ae1fefc91394d363abc6fb7e4
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 02:45:45 -0500

    destructuring arrays

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [ first, second, third ] = [ "Alex", "Ali", "Anna" ]
+// Destructuring arrays
+// console.log(first)
+// console.log(second)
+// console.log(third)
 ReactDOM.render(
   <React.StrictMode>
-    <App name="Wayne" />
+    <App name={ third } />
   </React.StrictMode>,
   document.getElementById('root')
 );
+if (module.hot) {
+  module.hot.accept();
+}
