053cbe010481ed1b9c1d06f1d0fe53894d97fbbf
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 09:50:56 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { FaStar } from 'react-icons/fa'
 import './index.css';
 
+const createArray = (length) => [
+  ...Array(length)
+]
+
+function Star({ selected = false, onSelect }) {
+  return (<FaStar
+    color={ selected ? 'red' : 'blue' }
+    onClick={ onSelect }
+  />
+  )
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [ selectedStars, setSelectedStars ] = useState(0)
+  return createArray(totalStars).map((n, i) => (
+    <Star
+      key={ i }
+      selected={ selectedStars > i }
+      onSelect={ () => setSelectedStars(i + 1) } />
+  ))
+}
+
+const App = () => {
+  return <StarRating totalStars={ 10 } />
+}
 
 
 ReactDOM.render(
