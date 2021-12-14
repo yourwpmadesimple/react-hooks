258e0acac1698c8f46be41c041aee85621572f6b
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 03:52:00 -0500

    examples of useState and destructuring useState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+function App() {
+  // const result = useState();
+  // console.log(result)
+
+  const [ status, setStatus ] = useState('Not Delivered')
+
+  console.log(status)
+  return (
+    <div>
+      <h1>The package is: { status }</h1>
+      <button onClick={ () => setStatus("Delivered") }>Deliver</button>
+    </div>
+  )
+}
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="Wayne" />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
