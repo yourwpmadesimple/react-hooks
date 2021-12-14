0f5044f208e58069541c7a4e6ba2563f90189fa0
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 08:21:19 -0500

    final result for 03_ComponentTrees

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { FaStar } from 'react-icons/fa';
 import './index.css';
 
+const createArray = (length) => [
+  ...Array(length)
+]
+
+function Star() {
+  return <FaStar />
+}
+
+function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => (
+    <Star key={ i } />
+  ))
+}
+
+const App = () => {
+  return <StarRating totalStars={ 1000 } />
+}
 
 
 ReactDOM.render(
