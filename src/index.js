997ed2b3be1daea29291e5d41ceae579cb76d650
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 03:55:12 -0500

    first commit, 02_useStateCheckBox branch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 
-function App() {
-  // const result = useState();
-  // console.log(result)
-
-  const [ status, setStatus ] = useState('Not Delivered')
-
-  console.log(status)
-  return (
-    <div>
-      <h1>The package is: { status }</h1>
-      <button onClick={ () => setStatus("Delivered") }>Deliver</button>
-    </div>
-  )
-}
 
 
 ReactDOM.render(
