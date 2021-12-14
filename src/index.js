05bc5d4f7d876cbaef060f40ad7236ab07a54280
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 10:04:23 -0500

    first commit, 05_Introducing_useEffect branch

--- src/index.js
+++ src/index.js
@@ -3,30 +3,12 @@ import ReactDOM from 'react-dom';
 import { FaStar } from 'react-icons/fa'
 import './index.css';
 
-const createArray = (length) => [
-  ...Array(length)
-]
-
-function Star({ selected = false, onSelect }) {
-  return (<FaStar
-    color={ selected ? 'red' : 'blue' }
-    onClick={ onSelect }
-  />
-  )
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [ selectedStars, setSelectedStars ] = useState(0)
-  return createArray(totalStars).map((n, i) => (
-    <Star
-      key={ i }
-      selected={ selectedStars > i }
-      onSelect={ () => setSelectedStars(i + 1) } />
-  ))
-}
-
 const App = () => {
-  return <StarRating totalStars={ 10 } />
+  return (
+    <secttion>
+      <p>Congratulations</p>
+    </secttion>
+  )
 }
 
 
