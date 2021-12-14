f653e393c65724fb91c236753c39aa70fca2374a
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 02:33:11 -0500

    using destructuring technique

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
+function App({ name }) {
+  // console.log(props)
+  // console.log(name)
   return (
     <div className="App">
-      <h1>Hello { props.name }</h1>
+      <h1>Hello { name }</h1>
     </div>
   );
 }
