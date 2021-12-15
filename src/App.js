67fb10c9d7127a1e16bae419759fd754a007f1fc
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 23:43:29 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useReducer } from "react";
 
-function App() {
 
+function App() {
+  const [ number, setNumber ] = useReducer((
+    number, newNumber) => number + newNumber,
+    0
+  );
   return (
     <div>
-      <h1 className="center">const [state, dispatch] = useReducer(reducer, initialState, init)</h1>
+      <h1
+        className="center"
+        onClick={ () => setNumber(1) }
+      >
+        { number }
+      </h1>
     </div>
   );
 }
