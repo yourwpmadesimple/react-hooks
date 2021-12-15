1ea45188acce0329f8b0e6f9846304b236c2c222
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 22:46:23 -0500

    first commit, 08_HandlingComplexStateWith_useReducer branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import React, {
-  useState,
-  useEffect
-} from "react";
+import React, { useReducer } from "react";
 
 function App() {
-  const [ data, setData ] = useState([])
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-      .then(response => response.json())
-      .then(setData)
-  }, []);
-
-  if (data) {
-    return (
-      <div>
-        <ul className="list-disc">
-          { data.map((user) => (
-            <li key={ user.id }>{ user.login }</li>
-          )) }
-        </ul>
-        <button
-          className="black"
-          onClick={ () => data ? setData([]) : setData }
-        >
-          Remove Data
-        </button>
-      </div>
-
-    )
-  }
 
   return (
     <div>
-      <h1 className="center">No Users</h1>
+      <h1 className="center">const [state, dispatch] = useReducer(reducer, initialState, init)</h1>
     </div>
   );
 }
