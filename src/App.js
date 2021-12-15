8fe784ee50c64949a0d689a2fa770d66ebb711cb
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-14 21:04:48 -0500

    initial and final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-
-import Navigation from '../src/navigation/navigation';
+import React, { useState, useEffect } from "react";
+//import './styles/index.css'
 
 function App() {
+  const [ name, setName ] = useState('Jan');
+  const [ admin, setAdmin ] = useState(false);
+
+  useEffect(() => {
+    console.log(`Celebrate ${name}`)
+  }, [ name ])
+
+  useEffect(() => {
+    console.log(`
+    The user is: ${admin ? "admin" : "not admin"},
+    `);
+  }, [ admin ])
+
   return (
-    <div>
-      <h1 className='text-center text-5xl my-4'>Bookkeeper</h1>
-      <nav
-        className="flex sm:justify-center space-x-4"
-      >
-        <Navigation />
-      </nav>
-    </div>
-  );
+    <section>
+      <p>Congratulations { name }!</p>
+      <button
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        type="button"
+        onClick={ () => setName("Shelly") }>
+        Change Winner
+      </button>
+      <p>{ admin ? "Logged in" : "not logged in" }</p>
+      <button
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        type="button"
+        onClick={ () => setAdmin(true) }>
+        Log in
+      </button>
+    </section>
+  )
 }
 
 export default App;
