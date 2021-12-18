4dbbbebae70d7f0ff0ae4ffb56990618d99b20dc
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-18 16:31:27 -0500

    final with readme notes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-import React, { createContext, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from "./App";
 
-const TreesContext = createContext();
-
-// useTrees is the custom hook
-export const useTrees = () => useContext(TreesContext);
-
-
-const trees = [
-  { id: 1, type: "Maple" },
-  { id: 2, type: "Oak" },
-  { id: 3, type: "Family" },
-  { id: 4, type: "Component" },
-]
-
 ReactDOM.render(
-  <TreesContext.Provider value={ { trees } }>
+  <React.StrictMode>
     <div className='container mx-auto px-1'>
       <App />
     </div>
-  </TreesContext.Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
---
+++ src/useFetch.jsx
@@ -0,0 +1,21 @@
+// if the data isn't available yet but is loading
+// if we get the data
+// it there's an error
+
+import { useState, useEffect } from "react";
+
+export function useFetch(uri) {
+  const [ data, setData ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(null);
+
+  useEffect(() => {
+    if (!uri) return;
+    fetch(uri)
+      .then((data) => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError)
+  }, [ uri ])
+  return { loading, data, error }
+}
