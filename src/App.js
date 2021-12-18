3a977e4c4f2b8f7863668ea05b39de84ff25db93
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-18 09:05:21 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { useReducer } from "react";
 import './styles/index.css'
 
+const initialState = {
+  message: "hi"
+}
+
+const reducer = ((state, action) => {
+  switch (action.type) {
+    case "yell":
+      return {
+        message: `HEY! I just said ${state.message}`
+      };
+    case "whisper":
+      return {
+        message: `escuse me, I just said ${state.message}`
+      };
+    default:
+      return {
+        message: "Nothing to see here"
+      }
+  }
+})
+
 function App() {
-  const [ checked, toggle ] = useReducer(
-    (checked) => !checked,
-    false
+  const [ state, dispatch ] = useReducer(
+    reducer,
+    initialState
   );
   return (
     <div>
-      <input
-        className="app-checkbox"
-        type="checkbox"
-        value={ checked }
-        onClick={ toggle }
-      />
-      { checked ? "checked" : "not checked" }
+      <p>Message: { state.message }</p>
+      <button
+        className="black"
+        onClick={ () => dispatch({ type: "yell" }) }>YELL
+      </button>
+      <button
+        className="black"
+        onClick={ () => dispatch({ type: "whisper" }) }>whisper
+      </button>
     </div>
   );
 }
